e75ab4736455ec9a8e2bdd9822ec8635f7dcbe65
lealobanov <lea.lobanov@icloud.com>
2023-10-26 00:27:16 +0900

    Create filters and assign difficulty

--- index.js
+++ index.js
@@ -22,4 +22,7 @@ export const mintingAMomentInTopShotSet = {
   smartContractExplanation: smartContractExplanationPath,
   transactionCode: transactionPath,
   transactionExplanation: transactionExplanationPath,
+  filters: {
+    difficulty: "intermediate"
+  }
 };
